683145e975e389d46ecfd4bb338254ed8a05f758
JonesRobM <k1899676@kcl.ac.uk>
2025-06-05 22:55:03 +0100

    building training utilities

---
+++ src/training/train_utils.py
@@ -0,0 +1,12 @@
+def save_checkpoint(model, optimizer, epoch, path):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict()
+    }, path)
+
+def load_checkpoint(model, optimizer, path):
+    ckpt = torch.load(path)
+    model.load_state_dict(ckpt['model_state_dict'])
+    optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+    return ckpt['epoch']
